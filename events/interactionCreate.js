0e5c55ecba9d8f191c2b726d0e403cafb589e3dd
Kevin | Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-07-23 12:03:46 -0700

    Initial Minor Edits.
    - Updated the Discord.js version.
    
    - Updated a slash interaction.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (interaction.isMessageComponent()) {console.log('A message component interaction was triggered.');}
 		if (interaction.isSelectMenu()) {console.log('A select menu interaction was triggered.');}
 
-		if (interaction.commandName === 'ping') {await interaction.reply({ content: `**Websocket Ping**: \`${Math.round(interaction.client.ws.ping)}ms\`.` });}
+		if (interaction.commandName === 'ping') {await interaction.reply({ content: `**Ping**: \`${Math.round(interaction.client.ws.ping)}ms\`.` });}
 
 		if (interaction.commandName === 'uptime') {await interaction.reply({ content: `**Uptime**: \`${interaction.client.utils.parseTime(Math.round(interaction.client.uptime / 1000))}\`.` });}
 	},
