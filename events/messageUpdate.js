c46a1ddf441fe68404f945c4c5b1edf8dbb2c103
Kevin | Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-07-27 15:02:19 -0700

    Minor Addition
    Added the messageDelete event, and updated the spacing for the error webhook in index.js.

---
+++ events/messageUpdate.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'messageDelete',
+	async execute(oldMessage, newMessage) {
+		if (newMessage.author.bot || !newMessage.guild || newMessage.attachments.size >= 1 || newMessage.embeds.size >= 1) return;
+
+		const guildDB = await newMessage.client.db.guildDB(newMessage.guild.id);
+
+		const embed = new MessageEmbed()
+			.setAuthor(newMessage.author.tag, newMessage.author.displayAvatarURL({ dynamic: true, size: 1024 }))
+			.setDescription(`**Message updated at ${newMessage.channel.name}**`)
+			.addField('**Before**:', `${oldMessage.content}`, false)
+			.addField('**After**:', `${newMessage.content}`, false)
+			.setColor('BLUE')
+			.setFooter(`ID: ${newMessage.author.id}`);
+		const channel = newMessage.guild.channels.cache.get(guildDB.messageLogsChannel);
+		if (channel) {channel.send({ embeds: [embed] });}
+	},
+};
--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const client = new Client({
 });
 
 process.on('unhandledRejection', (err) => {
-	console.error(`${chalk.red.bold('[Node] Unhandled Promise Rejection:')}\n${err.stack}`);
-	client.errorWebhook.send({ content: `[Node] Unhandled Promise Rejection:')}\n${err.stack}` });
+	console.error(`${chalk.red.bold('[Node Error] Unhandled Promise Rejection:')}\n${err.stack}`);
+	client.errorWebhook.send({ content: `[Node Error] Unhandled Promise Rejection:')}\n\n\n${err.stack}` });
 });
 
 client.login(process.env.TOKEN);
