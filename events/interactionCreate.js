ed0042ca9c3fead42c9e9c0575b11e4e5cdd573d
Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-08-22 20:43:39 -0700

    Fix the for loop for checking color roles.
    Dedicated to Bro Community.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -117,13 +117,9 @@ module.exports = {
 				} else {
 					// The start of the check module.
 					const colors = ['862307659931582464', '862307660219809803', '864516405599928350', '862307661206126613', '864516756864499744', '862307660505153618', '862307661390675968', '864517904308371457', '862307661605502997', '862307662087192577', '863170781914726461', '862307662939291648', '862307663408136272', '864519101249486879', '862307663886286859', '862307664356048916', '862307664649125908', '862307664942596116', '864519114349477888', '862307665252974593', '864519485104717874', '862307665273946122', '862463644851896362', '862463669501296640', '862463660551438347'];
-					/* for (const mmLol of susb) {
-						if (!interaction.member.roles.cache.has(mmLol)) continue;
-						else return await interaction.reply({ content: 'You idiot! You can only have one color role..', ephemeral: true });
-					} */
 
 					for (const color of colors) {
-						if (colors.includes(role.id) && interaction.member.roles.cache.has(color)) return await interaction.reply({ content: 'Test' });
+						if (colors.includes(role.id) && interaction.member.roles.cache.has(color)) return await interaction.reply({ content: 'You idiot! You can only have one color role..', ephemeral: true });
 						else continue;
 					}
 
