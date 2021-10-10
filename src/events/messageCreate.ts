67f540c4766f3f8cc7d7955aa57652c62abaecab
Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-10-10 16:13:41 -0700

    Remove demo auto response.

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,24 +1,12 @@
-import { Message, MessageEmbed, TextChannel } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 import { ClientBase } from "../structures/Client";
 import { Event } from "../types";
-import { AutoResponseModel } from "../models/Autoresponse";
-import { bgRed } from "chalk";
 
 export = <Event> {
 	name: "messageCreate",
 	description: "Emitted whenever a message is created.",
 	async execute(message: Message, client: ClientBase) {
         if (!message.guild || message.author.bot || message.partial) return;
-
-        await AutoResponseModel.findOne({ guildID: message.guild.id }, (err: Error, docs: Array<string>) => {
-            if (err) console.error(bgRed(err.stack));
-
-            docs.map(async (doc: any) => {
-                if (message.content.match(new RegExp(message.content, "gmixs"))) {
-                    await (message.channel as TextChannel).send({ content: doc.content });
-                }
-            });
-        });
 
         const embed = new MessageEmbed()
 			.setTitle(`Hello ${message.author.username}!`)
