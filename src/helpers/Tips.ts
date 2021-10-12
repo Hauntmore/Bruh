aca0a6887d557a9da0ccaad84438301ac7375df2
Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-10-11 21:30:38 -0700

    Commit a function for tip messages with files.

--- src/helpers/Tips.ts
+++ src/helpers/Tips.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, MessageActionRow, MessageEmbed } from "discord.js";
+import { CommandInteraction, MessageActionRow, MessageAttachment, MessageEmbed } from "discord.js";
 import { tips } from "../config/Config";
 
 const random = Math.floor(Math.random() * 100) + 2;
@@ -31,6 +31,23 @@ export async function contentTipMessage(interaction: CommandInteraction, content
     } else {
         return await interaction.reply({
             content: content,
+            ephemeral: ephemeral,
+            components: components,
+        });
+    }
+}
+
+export async function fileTipMessage(interaction: CommandInteraction, files: Array<MessageAttachment>, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []) {
+    if (random >= 15 && random <= 25) {
+        return await interaction.reply({
+            content: `**Tip**: ${tip}`,
+            files: files,
+            ephemeral: ephemeral,
+            components: components,
+        });
+    } else {
+        return await interaction.reply({
+            files: files,
             ephemeral: ephemeral,
             components: components,
         });
