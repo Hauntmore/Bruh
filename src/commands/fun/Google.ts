541edcd1a348cbf82f9a3cc8cc703fa50ce537ea
Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-10-13 20:57:44 -0700

    Google command for normies to search 1 + 1

---
+++ src/commands/fun/Google.ts
@@ -0,0 +1,22 @@
+import { CommandInteraction } from "discord.js";
+import { Command } from "../../types";
+
+export = <Command> {
+	name: "google",
+	description: "Ask her something.",
+	usage: "<--search>",
+    options: [
+		{
+			name: "search",
+			type: 3,
+			description: "The query to search on Google.",
+			required: true,
+		},
+	],
+	cooldown: 5,
+	async execute(interaction: CommandInteraction) {
+		const args = interaction.options.getString("search", true);
+
+        await interaction.reply({ content: `http://lmgtfy.com/?q=${args.split(" ").join("+")}` });
+	},
+};
