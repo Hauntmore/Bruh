7b2b0e3d9d00b0bebd0939d3f86dad2b6915a181
Hauntless <77118310+Hauntmore@users.noreply.github.com>
2021-10-16 11:25:58 -0700

    Annotate params

--- src/helpers/Tips.ts
+++ src/helpers/Tips.ts
@@ -4,7 +4,15 @@ import { tips } from "../config/Config";
 const random = Math.floor(Math.random() * 100) + 2;
 const tip = tips.random();
 
-export async function embedTipMessage(interaction: CommandInteraction, embeds: Array<MessageEmbed | Object>, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []) {
+/**
+ * 
+ * @param {CommandInteraction} interaction The CommandInteraction class.
+ * @param {MessageEmbed[] | Object[]} embeds The array of embeds or embed objects to send.
+ * @param {boolean} ephemeral Whether or not to send the interaction as ephemeral.
+ * @param {MessageActionRow[]} components Array of MessageActionRows.
+ * @returns {Promise<void>} A void promise.
+ */
+export async function embedTipMessage(interaction: CommandInteraction, embeds: Array<MessageEmbed | Object>, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []): Promise<void> {
     if (random >= 15 && random <= 25) {
         return await interaction.reply({
             content: `**Tip**: ${tip}`,
@@ -21,7 +29,15 @@ export async function embedTipMessage(interaction: CommandInteraction, embeds: A
     }
 }
 
-export async function contentTipMessage(interaction: CommandInteraction, content: string, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []) {
+/**
+ * 
+ * @param {CommandInteraction} interaction The CommandInteraction class.
+ * @param {string} content The content to send.
+ * @param {boolean} ephemeral Whether or not to send the interaction as ephemeral.
+ * @param {MessageActionRow[]} components Array of MessageActionRows.
+ * @returns {Promise<void>} A void promise.
+ */
+export async function contentTipMessage(interaction: CommandInteraction, content: string, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []): Promise<void> {
     if (random >= 15 && random <= 25) {
         return await interaction.reply({
             content: `${content}\n\n**Tip**: ${tip}`,
@@ -37,7 +53,15 @@ export async function contentTipMessage(interaction: CommandInteraction, content
     }
 }
 
-export async function fileTipMessage(interaction: CommandInteraction, files: Array<MessageAttachment>, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []) {
+/**
+ * 
+ * @param {CommandInteraction} interaction The CommandInteraction class.
+ * @param {string} files The array of files to send.
+ * @param {boolean} ephemeral Whether or not to send the interaction as ephemeral.
+ * @param {MessageActionRow[]} components Array of MessageActionRows.
+ * @returns {Promise<void>} A void promise.
+ */
+export async function fileTipMessage(interaction: CommandInteraction, files: Array<MessageAttachment>, ephemeral?: boolean | false, components?: Array<MessageActionRow> | []): Promise<void> {
     if (random >= 15 && random <= 25) {
         return await interaction.reply({
             content: `**Tip**: ${tip}`,
